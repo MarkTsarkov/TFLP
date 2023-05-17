de99206c6c469acbecbe7dfa0d4fcd6cddffb1ad
Mark <ml.tsarkovg@gmail.com>
2023-05-18 01:54:37 +0300

    git actions problem resolving: added secretKey

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,6 +7,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
+
+
 
   // This widget is the root of your application.
   @override
